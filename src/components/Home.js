0cc02b88de9b12e9e0d0e89a1c7dc49fd8dad1da
xm2by <mxxumin@163.com>
2021-08-01 15:50:21 +0800

    feat: 添加演示demo效果图

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,9 @@ const Home = () => {
   const [showImg, setShowImg] = useState(false);
   return (
     <div>
-      <button onClick={() => setShowImg(true)}>展示人类高质量男性</button>
+      <button onClick={() => setShowImg(true)} style={{ margin: "32px 0" }}>
+        展示人类高质量男性
+      </button>
       {showImg && <EliteImg></EliteImg>}
     </div>
   );
